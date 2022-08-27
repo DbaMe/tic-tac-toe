5c948be8c9cfe9be1e8c649c53e2925c6eab7fb9
Hammed Waheed <worldwid01@gmail.com>
2022-08-28 01:09:27 +0200

    props

--- src/components/Square.js
+++ src/components/Square.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Square(props) {
   return (
-    <button className="square" onClick={props.onClick}>
+    <button className="square" onClick={ props.onClick}>
         {props.value}
     </button>
   )
